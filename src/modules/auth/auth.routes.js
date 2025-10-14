a373843288f9011fee97a4bb31cf146a126fbcf0
khanhpdbth00867 <khanhpdbth00867@fpt.edu.vn>
2025-10-14 17:15:34 +0700

    them api quen mat khau

--- src/modules/auth/auth.routes.js
+++ src/modules/auth/auth.routes.js
@@ -1,10 +1,16 @@
 import { Router } from "express";
-import { login, register, verifyUser } from "./auth.controller.js";
+import {
+  login,
+  register,
+  verifyUser,
+  resetPassword,
+} from "./auth.controller.js";
 
 const authRoute = Router();
 
 authRoute.post("/register", register);
 authRoute.post("/login", login);
 authRoute.get("/verify/:token", verifyUser);
+authRoute.post("/reset-password", resetPassword);
 
 export default authRoute;
