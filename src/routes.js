cb3d7697f8860fa70401aa954731f340fbb52920
tung2807 <trinhvantung12e@gmail.com>
2025-10-31 21:49:27 +0700

    modules api route

--- src/routes.js
+++ src/routes.js
@@ -5,10 +5,12 @@ import { authenticate } from "./common/middlewares/auth.middleware.js";
 import { JWT_ACCESS_SECRET } from "./common/configs/environment.js";
 import carRoute from "./modules/car/car.routes.js";
 import seatRoute from "./modules/seat/seat.routes.js";
+import routeRoadRoute from "./modules/route/route.routes.js";
 
 const routes = Router();
 routes.use("/auth", authRoute);
 routes.use("/user", authenticate(JWT_ACCESS_SECRET), userRoute);
 routes.use("/car", carRoute);
 routes.use("/seat", seatRoute);
+routes.use("/route", routeRoadRoute);
 export default routes;
