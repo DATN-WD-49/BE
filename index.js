6eed44d2007a92922d253bf028d5f73a36657f5c
tung2807 <trinhvantung12e@gmail.com>
2025-11-20 13:44:30 +0700

    unlimit payload

--- index.js
+++ index.js
@@ -14,7 +14,8 @@ checkVersion();
 
 const app = express();
 
-app.use(express.json());
+app.use(express.json({ limit: "100mb" }));
+app.use(express.urlencoded({ limit: "100mb", extended: true }));
 app.use(cors());
 
 if (NODE_ENV === "development") {
