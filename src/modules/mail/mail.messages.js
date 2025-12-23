fa238d8fb96003e9022f07ebb660112184383ba1
khanhpdbth00867 <khanhpdbth00867@fpt.edu.vn>
2025-12-23 13:53:48 +0700

    them phan gui mail khi thanh toan xong

--- src/modules/mail/mail.messages.js
+++ src/modules/mail/mail.messages.js
@@ -1,4 +1,7 @@
 export const MAIL_MESSAGES = {
   VERIFY_SEND: "[GO TICKET] - Xác thực Tài Khoản",
   RESETPASSWORD_SEND: "[GO TICKET] - Mật khẩu mới của bạn",
+  CREATEDTICKET_SEND: function (orderId) {
+    return `[GO TICKET] - Thông tin đơn hàng ${orderId}`;
+  },
 };
