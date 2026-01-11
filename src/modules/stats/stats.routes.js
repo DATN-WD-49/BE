6d3d7bd92ce092d2679322f3cd914896dca78f4d
khanhpdbth00867 <khanhpdbth00867@fpt.edu.vn>
2026-01-12 05:03:29 +0700

    them top tuyen duong theo doanh thu

--- src/modules/stats/stats.routes.js
+++ src/modules/stats/stats.routes.js
@@ -1,11 +1,17 @@
 import { Router } from "express";
-import { getOverview, getOverviewByMonth } from "./stats.controller.js";
+import {
+  getOverview,
+  getOverviewByMonth,
+  getTopRevenueRoute,
+} from "./stats.controller.js";
 import ticketStatsRoute from "./ticketstats/ticketstats.routes.js";
 
 const statsRoute = Router();
 
 statsRoute.get("/overview", getOverview);
 statsRoute.get("/overview/month-of-year", getOverviewByMonth);
+statsRoute.get("/overview/revenue-route", getTopRevenueRoute);
+
 statsRoute.use("/ticket", ticketStatsRoute);
 
 export default statsRoute;
