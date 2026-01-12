a62ce49fa2724007beafbcc9d2368c9fbad33e2a
khanhpdbth00867 <khanhpdbth00867@fpt.edu.vn>
2026-01-12 11:01:18 +0700

    ap dung cac cron vua them

--- src/modules/job/index.js
+++ src/modules/job/index.js
@@ -1,5 +1,15 @@
+import { startOrdersExpiredJob } from "./order.expired.js";
+import { startcompletedSchedulesJob } from "./schedule.completed.js";
+import { startprepareSchedulesJob } from "./schedule.prepare.js";
+import { startRunningSchedulesJob } from "./schedule.running.js";
 import { startSeatExpiredJob } from "./seat.expired.js";
+import { startLockUsersExpiredJob } from "./user.lock.expired.js";
 
 export const startJob = () => {
   startSeatExpiredJob();
+  startOrdersExpiredJob();
+  startprepareSchedulesJob();
+  startRunningSchedulesJob();
+  startcompletedSchedulesJob();
+  startLockUsersExpiredJob();
 };
